22d1fbd1a9d169aa66549efbedbbb140654a3d4b
sina-razaghi <sina.razaghi79@gmail.com>
2024-02-07 22:45:07 +0330

    first

--- FirstAPI.py
+++ FirstAPI.py
@@ -1,12 +1,26 @@
-from anyio import Path
+from enum import Enum
 from fastapi import FastAPI
 
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
 app = FastAPI()
+
+
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/home")
+async def home():
+    return {"message": "Home Page"}
+
 
 @app.get("/login/{user_id}")
 async def read(user_id: int):
@@ -15,6 +29,28 @@ async def read(user_id: int):
             "message": "Welcome to home"
             }
 
-@app.post("/results")
+
+@app.get("/users")
+async def users():
+    return {"list": ["Rick", "Morty", "John"]}
+
+
+@app.get("/panel")
+async def panel():
+    return {"id": "admin"}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+    
+    if model_name.value == "lenet":
+        return {"model_name": model_name, "message": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residauls"}
+
+
+@app.get("/results")
 async def results():
     return {"detail": "Somethings about app"}
